14e5bb6596e312915a67b2dd1dcdebd470c3be65
Mauro Felipe <mfelipxl@gmail.com>
2025-05-24 12:34:16 -0400

    Feat: finishing a page.

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,32 @@ function App() {
         <img
           src="/logo.png"
           alt=""
-          className="w-40 h-40 rounded-full mb-3 mx-auto border-4 border-slate-800 shadow-lg object-cover"
+          className="w-40 h-40 rounded-full mb-3 mx-auto border-4 border-stone-900 shadow-lg object-cover"
         />
         <h1 className="text-2xl font-bold text-white">Mauro Felipe</h1>
         <p className="text-slate-400 mb-5">Desenvolvedor Web • Fullstack</p>
 
-        <div className="space-y-3">
-          {links.map((link, idx) => (
-            <a
-              key={idx}
-              href={link.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={`block w-full py-3 rounded-xl font-semibold transition ${link.bg} flex justify-between items-center px-5`}
-            >
-              <span className='flex'>
-                {link.icon}
-              </span>
-              <span>
-                {link.label}
-              </span>
-            </a>
-          ))}
+        <div className="space-y-4">
+          {links.map((link, idx) => {
+            const Icon = link.icon
+            return(
+              <a
+                key={idx}
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`block w-full py-3 rounded-xl font-semibold transition-all duration-300 ease-in-out
+                ${link.bg} backdrop-blur-md hover:scale-105 flex justify-between items-center px-5`}
+              >
+                <span className='flex'>
+                  <Icon />
+                </span>
+                <span>
+                  {link.label}
+                </span>
+              </a>
+            )
+          })}
         </div>
 
         <footer className="text-sm text-slate-500 mt-6">
--- src/data/links.js
+++ src/data/links.js
@@ -2,25 +2,25 @@ import { Github, Instagram, Linkedin, Youtube } from 'lucide-react'
 
 export const links = [
   {
-    icon: <Github className="w-5 h-5" />,
+    icon: Github,
     label: 'GitHub',
     url: 'https://github.com/lopesmauro',
     bg: 'bg-gradient-to-r from-violet-800 to-purple-900 text-white',
   },
   {
-    icon: <Instagram className="w-4 h-4" />,
+    icon: Instagram,
     label: 'Instagram',
     url: 'https://www.instagram.com/themfcode/?hl=pt',
     bg: 'bg-gradient-to-r from-pink-500 to-yellow-500 text-white',
   },
   {
-    icon: <Linkedin className="w-5 h-5" />,
+    icon: Linkedin,
     label: 'LinkedIn',
     url: 'https://www.linkedin.com/in/mauro-felipe-lopes/',
     bg: 'bg-blue-600 text-white',
   },
   {
-    icon: <Youtube className="w-5 h-5" />,
+    icon: Youtube,
     label: 'Youtube',
     url: 'https://linkedin.com/in/seunome',
     bg: 'bg-gradient-to-r from-red-800 to-red-500 text-white',
